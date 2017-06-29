734b1731ab95862ea14ba84b217304bf0c6b1799
Andrea <andrea.93@hotmail.it>
2017-06-29 17:03:20 +0200

    mobile try

--- src/app.js
+++ src/app.js
@@ -3,3 +3,5 @@ require('aframe-extras');
 require('super-hands');
 require('aframe-teleport-controls');
 require('aframe-physics-components');
+var physics = require('aframe-physics-system');
+physics.registerAll();
