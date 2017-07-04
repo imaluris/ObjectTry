24de1c294d4f972415adcd380080686c7559e9c6
iMaLuRiS <andrea.castellaniperelli@gmail.com>
2017-07-04 17:46:08 +0200

    divided by object

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require('aframe');
-require('aframe-extras');
+require('aframe-extras').registerAll();
 require('super-hands');
 require('aframe-teleport-controls');
-require('aframe-physics-system');
+// require('aframe-physics-components');
+var physics = require('aframe-physics-system');
+physics.registerAll();
